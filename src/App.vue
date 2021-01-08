83d13c1a8371bde9f7860ac3a8e9a5b98799880d
Tim Millard <timwmillard@gmail.com>
2021-01-08 21:14:39 +1100

    Created layout

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,44 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="container">
+    <section class="section">
+      <h1 class="title is-1">InspireMe</h1>
+      <div class="columns">
+        <div class="column is-half">
+            <b-field label="Message">
+            <b-input maxlength="200" type="textarea" placeholder="Enter your message"></b-input>
+        </b-field>
+        </div>
+        <div class="column is-half">
+              <b-field label="Background URL">
+                <b-input v-model="name" type="url" placeholder="Enter bacgkround URL"></b-input>
+              </b-field>
+        </div>
+      </div>
+      <div class="column is-full">
+        <b-button type="is-primary" size="is-medium" @click="generateImage">Generate Image</b-button>
+      </div>
+    </section>
+
+    <section class="section">
+      <b-image
+            :src="require('@/assets/logo.png')"
+            alt="Your Message"
+            ratio="601by235"
+        ></b-image>
+    </section>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+  },
+  methods: {
+    generateImage() {
+      this.$buefy.notification.open('Clicked')
+    }
   }
 }
 </script>
@@ -23,6 +50,5 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
